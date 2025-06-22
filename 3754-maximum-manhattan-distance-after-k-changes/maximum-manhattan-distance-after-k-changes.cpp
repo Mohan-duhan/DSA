6c46aa50b20a880d85dc88cb5d66af3c30b1ca25
Mohan duhan <135856544+Mohan-duhan@users.noreply.github.com>
2025-06-22 14:34:56 +0530

    Time: 55 ms (71.34%) | Memory: 38.2 MB (59.11%) - LeetSync

--- 3754-maximum-manhattan-distance-after-k-changes/maximum-manhattan-distance-after-k-changes.cpp
+++ 3754-maximum-manhattan-distance-after-k-changes/maximum-manhattan-distance-after-k-changes.cpp
@@ -3,23 +3,29 @@ public:
     int maxDistance(string s, int k) {
         int n = s.length();
         int south = 0, north = 0, west = 0, east = 0;
-        int MD = 0;
+        int Mah_Dis = 0;
         int ans = 0;
 
-        for(int i = 0;i<n;i++){
+        for (int i = 0; i < n; i++) {
             char ch = s[i];
-            if(ch == 'N') north++;
-            if(ch == 'W') west++;
-            if(ch == 'S') south++;
-            if(ch == 'E') east++;
+            if (ch == 'N')
+                north++;
+            if (ch == 'W')
+                west++;
+            if (ch == 'S')
+                south++;
+            if (ch == 'E')
+                east++;
 
-        int x = abs(north - south);
-        int y = abs(east - west);
-        MD = x + y;
+            int x = abs(north - south);
+            int y = abs(east - west);
+            Mah_Dis = x + y;
 
-        int dis = MD + min(2 * k, i + 1 - MD);
+            int wasted = i + 1 - Mah_Dis;
 
-        ans = max(dis,ans);
+            int dis = Mah_Dis + min(2 * k, wasted);
+
+            ans = max(dis, ans);
         }
         return ans;
     }

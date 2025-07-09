3570881aa2054eabb5534f4853b9512e5b18d5c5
Mohan duhan <135856544+Mohan-duhan@users.noreply.github.com>
2025-07-09 18:52:02 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (82.88%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     void backtrack(vector<int>& candidates, int target, int idx,
-                   vector<int> check_comb, vector<vector<int>>& res) {
+                   vector<int>& check_comb, vector<vector<int>>& res) {
         if (target == 0) {
             res.push_back(check_comb);
             return;
@@ -12,6 +12,7 @@ public:
                 continue;
             if (candidates[i] > target)
                 break;
+
             check_comb.push_back(candidates[i]);
             backtrack(candidates, target - candidates[i], i + 1, check_comb, res);
             check_comb.pop_back();
@@ -19,7 +20,7 @@ public:
     }
 
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        sort(candidates.begin(), candidates.end());
+        sort(candidates.begin(), candidates.end());  
         vector<vector<int>> res;
         vector<int> check_comb;
         backtrack(candidates, target, 0, check_comb, res);

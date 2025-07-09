ad6eb702f2bec80f1f95ebce3518a9db2681d340
Mohan duhan <135856544+Mohan-duhan@users.noreply.github.com>
2025-07-09 19:55:07 +0530

    Time: 10 ms (33.09%) | Memory: 15.1 MB (23.51%) - LeetSync

--- 47-permutations-ii/permutations-ii.cpp
+++ 47-permutations-ii/permutations-ii.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
-    vector<vector<int>> res;
-
-    void backtrack(vector<int> nums, int idx) {
+    void backtrack(vector<int>& nums, int idx, vector<vector<int>>& res) {
         if (idx == nums.size()) {
             res.push_back(nums);
             return;
         }
 
+        unordered_set<int> set;
+
         for (int i = idx; i < nums.size(); i++) {
-            if (i == idx || nums[i] != nums[idx]) {
-                swap(nums[idx], nums[i]);
-                backtrack(nums, idx + 1);
-            }
+            if (set.count(nums[i])) continue; 
+            set.insert(nums[i]);
+
+            swap(nums[idx], nums[i]);
+            backtrack(nums, idx + 1, res);
+            swap(nums[idx], nums[i]); 
         }
     }
 
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        backtrack(nums, 0);
+        sort(nums.begin(), nums.end()); 
+        vector<vector<int>> res;
+        backtrack(nums, 0, res);
         return res;
     }
 };

acc0faf3c964245e29c2308029d28a0aae26b164
Mohan duhan <135856544+Mohan-duhan@users.noreply.github.com>
2025-07-09 10:08:54 +0530

    Time: 1 ms (54.58%) | Memory: 8.9 MB (64.33%) - LeetSync

--- 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.cpp
+++ 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.cpp
@@ -7,11 +7,7 @@ public:
             int n = s.length();
 
             for (int i = 0; i < n; i++) {
-                char ch = s[i] == 'z'
-                              ? 'a'
-                              : s[i] + 1; // agar 'z' aajaye toh uska next char
-                                          // 'a' hoga otherwise string me append
-                                          // karte raho uske next element ko.
+                char ch = s[i] == 'z' ? 'a' : s[i] + 1;
                 s.push_back(ch);
             }
         }

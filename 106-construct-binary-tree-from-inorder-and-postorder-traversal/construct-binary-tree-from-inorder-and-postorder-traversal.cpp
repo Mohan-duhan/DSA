3dc0c178cd471cd9abeaab2fedccb11aacf87427
Mohan duhan <135856544+Mohan-duhan@users.noreply.github.com>
2025-07-16 14:52:20 +0530

    Time: 3 ms (71.23%) | Memory: 27.2 MB (85.54%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -1,30 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
 class Solution {
 public:
     TreeNode* solve(vector<int>& inorder, vector<int>& postorder, int start,
-                    int end, int& idx, unordered_map<int, int>& inMap) {
+                    int end, int& idx) {
         if (start > end) {
             return NULL;
         }
 
-        int rootval = postorder[idx--];
+        int rootval = postorder[idx];
+        int i = start;
+
+        for (; i <= end; i++) {
+            if (inorder[i] == rootval)
+                break;
+        }
+
+        idx--;
         TreeNode* root = new TreeNode(rootval);
 
-        int i = inMap[rootval];
-        root->right = solve(inorder, postorder, i + 1, end, idx, inMap);
-        root->left = solve(inorder, postorder, start, i - 1, idx, inMap);
+        // Build right subtree first (due to postorder being traversed from end)
+        root->right = solve(inorder, postorder, i + 1, end, idx);
+        root->left = solve(inorder, postorder, start, i - 1, idx);
 
         return root;
     }
 
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
         int n = postorder.size();
-        unordered_map<int, int> inMap;
-
-        for (int i = 0; i < n; ++i) {
-            inMap[inorder[i]] = i;
-        }
 
         int idx = n - 1;
-        return solve(inorder, postorder, 0, n - 1, idx, inMap);
+        return solve(inorder, postorder, 0, n - 1, idx);
     }
 };

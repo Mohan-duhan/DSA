e33b180ba76446504dfa58f4ac7ab73b6e41d6ba
Mohan duhan <135856544+Mohan-duhan@users.noreply.github.com>
2025-07-21 17:18:05 +0530

    Time: 5 ms (100.00%) | Memory: 126.2 MB (41.62%) - LeetSync

--- 3936-split-array-by-prime-indices/split-array-by-prime-indices.cpp
+++ 3936-split-array-by-prime-indices/split-array-by-prime-indices.cpp
@@ -1,36 +1,28 @@
 class Solution {
 public:
-    bool isPrime(int n) {
-        if (n <= 1)
-            return false;
-        for (int i = 2; i * i <= n; i++) {
-            if (n % i == 0)
-                return false;
-        }
-        return true;
-    }
-
     long long splitArray(vector<int>& nums) {
-        vector<int> A;
-        vector<int> B;
-
         int n = nums.size();
-        for (int i = 0; i < n; i++) {
-            if (isPrime(i)) {
-                A.push_back(nums[i]);
-            } else {
-                B.push_back(nums[i]);
+        vector<bool> isPrime(n, true);
+
+        if (n > 0)
+            isPrime[0] = false;
+        if (n > 1)
+            isPrime[1] = false;
+
+        for (int i = 2; i * i < n; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j < n; j += i) {
+                    isPrime[j] = false;
+                }
             }
         }
-
         long long sum_A = 0, sum_B = 0;
-
-        for (int x : A)
-            sum_A += x;
-        for (int x : B)
-            sum_B += x;
-
-        long long diff = abs(sum_A - sum_B);
-        return min(diff, 100000000000000LL);
+        for (int i = 0; i < n; i++) {
+            if (isPrime[i])
+                sum_A += nums[i];
+            else
+                sum_B += nums[i];
+        }
+        return abs(sum_A - sum_B);
     }
 };

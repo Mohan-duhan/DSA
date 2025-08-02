c3f2b32e76bff0cdd5a0cc4d105d6bdcda4cb19c
Mohan duhan <135856544+Mohan-duhan@users.noreply.github.com>
2025-08-02 15:26:29 +0530

    Time: 33 ms (80.44%) | Memory: 53.5 MB (85.39%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -3,7 +3,9 @@ public:
     bool isPalindrome(const string& str) {
         int l = 0, r = str.size() - 1;
         while (l < r) {
-            if (str[l++] != str[r--]) return false;
+            if (str[l] != str[r]) return false;
+            l++;
+            r--;
         }
         return true;
     }

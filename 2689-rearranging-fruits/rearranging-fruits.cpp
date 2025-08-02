1b576ab7b80eaae3ab46b482bf672f1e96bdad94
Mohan duhan <135856544+Mohan-duhan@users.noreply.github.com>
2025-08-02 16:22:26 +0530

    Time: 53 ms (62.07%) | Memory: 89.9 MB (76.55%) - LeetSync

---
+++ 2689-rearranging-fruits/rearranging-fruits.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    long long minCost(vector<int>& basket1, vector<int>& basket2) {
+        sort(basket1.begin(), basket1.end());
+        sort(basket2.begin(), basket2.end());
+
+        unordered_map<int, int> freq;
+        vector<int> store;
+        int minEl = INT_MAX;
+
+        for (int i = 0; i < basket1.size(); i++) {
+            freq[basket1[i]]++;
+            freq[basket2[i]]--;
+            minEl = min({minEl, basket1[i], basket2[i]});
+        }
+
+        for (auto& [k, v] : freq) {
+            if (v % 2 != 0)
+                return -1;
+            for (int i = 0; i < abs(v) / 2; i++) {
+                store.push_back(k);
+            }
+        }
+        sort(store.begin(), store.end());
+
+        long long  cost = 0;
+        for (int i = 0; i < store.size() / 2; i++) {
+            cost += min((long long)2 * minEl, (long long)store[i]);
+        }
+        return cost;
+    }
+};

d7091af9612bc8e488f7179b23d8b3cad699fbf8
Mohan duhan <135856544+Mohan-duhan@users.noreply.github.com>
2025-08-14 10:08:01 +0530

    Time: 3 ms (12.19%) | Memory: 9.2 MB (22.96%) - LeetSync

--- 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.cpp
+++ 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.cpp
@@ -5,11 +5,10 @@ public:
         int n = num.size();
 
         for (int i = 0; i <= n - 3; i++) {
-            string res = num.substr(i, 3);
-            if (res == "999" || res == "888" || res == "777" ||
-                res == "666" || res == "555" || res == "444" ||
-                res == "333" || res == "222" || res == "111" || res == "000") {
-                if (res > ans) ans = res;
+            if (num[i] == num[i + 1] && num[i] == num[i + 2]) {
+                string res = num.substr(i, 3);
+                if (res > ans)
+                    ans = res;
             }
         }
         return ans;

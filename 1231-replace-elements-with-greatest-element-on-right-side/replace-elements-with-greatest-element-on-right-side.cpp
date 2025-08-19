9758b506b4cba29111f03c26b51a380331ed3889
Mohan duhan <135856544+Mohan-duhan@users.noreply.github.com>
2025-08-19 19:15:35 +0530

    Time: 1852 ms (10.24%) | Memory: 76.7 MB (8.47%) - LeetSync

--- 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.cpp
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.cpp
@@ -7,7 +7,7 @@ public:
         for (int i = 0; i < n; i++) {
             int max_val = -1;
             for (int j = i + 1; j < n; j++) {
-                    max_val = max(max_val, arr[j]);
+                max_val = max(max_val, arr[j]);
             }
             ans.push_back(max_val);
         }

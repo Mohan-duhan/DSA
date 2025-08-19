6456bc709fc8a1a2861f7946ca0f5fd40bb762c9
Mohan duhan <135856544+Mohan-duhan@users.noreply.github.com>
2025-08-19 19:15:10 +0530

    Time: 1825 ms (11.80%) | Memory: 76.4 MB (11.71%) - LeetSync

---
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> replaceElements(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> ans;
+
+        for (int i = 0; i < n; i++) {
+            int max_val = -1;
+            for (int j = i + 1; j < n; j++) {
+                    max_val = max(max_val, arr[j]);
+            }
+            ans.push_back(max_val);
+        }
+        return ans;
+    }
+};

85f3919a69e14a481c7a00f44aa140b3ba3becdf
Mohan duhan <135856544+Mohan-duhan@users.noreply.github.com>
2025-08-28 22:55:28 +0530

    Time: 191 ms (96.19%) | Memory: 131.9 MB (98.98%) - LeetSync

--- 3461-find-the-minimum-area-to-cover-all-ones-i/find-the-minimum-area-to-cover-all-ones-i.cpp
+++ 3461-find-the-minimum-area-to-cover-all-ones-i/find-the-minimum-area-to-cover-all-ones-i.cpp
@@ -5,23 +5,29 @@ public:
         int m = grid[0].size();
 
         int x_max = -1, y_max = -1;
-        int x_min = m,  y_min = n;   
+        int x_min = INT_MAX, y_min = INT_MAX;
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (grid[i][j] == 1) {
-                    if (j < x_min) x_min = j;
-                    if (j > x_max) x_max = j;
-                    if (i < y_min) y_min = i;
-                    if (i > y_max) y_max = i;
+                    x_max = max(x_max, j);
+                    x_min = min(x_min, j);
+
+                    y_max = max(y_max, i);
+                    y_min = min(y_min, i);
                 }
             }
         }
-
-        if (x_max == -1) return 0;
 
         int l = x_max - x_min + 1;
         int b = y_max - y_min + 1;
         return l * b;
     }
 };
+
+auto init = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();

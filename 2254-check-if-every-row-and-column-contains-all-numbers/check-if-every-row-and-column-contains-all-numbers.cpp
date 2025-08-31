5138d226bf28ccae1eef135af76e1f328b922e11
Mohan duhan <135856544+Mohan-duhan@users.noreply.github.com>
2025-08-31 13:46:19 +0530

    Time: 159 ms (25.60%) | Memory: 98.3 MB (10.74%) - LeetSync

--- 2254-check-if-every-row-and-column-contains-all-numbers/check-if-every-row-and-column-contains-all-numbers.cpp
+++ 2254-check-if-every-row-and-column-contains-all-numbers/check-if-every-row-and-column-contains-all-numbers.cpp
@@ -2,21 +2,27 @@ class Solution {
 public:
     bool checkValid(vector<vector<int>>& matrix) {
         int n = matrix.size();
+        int m = matrix[0].size();
 
+        //validate rows
         for (int i = 0; i < n; i++) {
-            set<int> rowSet;
-            set<int> colSet;
-
-            for (int j = 0; j < n; j++) {
-                // Check row
-                if (rowSet.find(matrix[i][j]) != rowSet.end()) 
+            set<int> st;
+            for (int j = 0; j < m; j++) {
+                if (st.find(matrix[i][j]) != st.end()) {
                     return false;
-                rowSet.insert(matrix[i][j]);
+                }
+                st.insert(matrix[i][j]);
+            }
+        }
 
-                // Check column
-                if (colSet.find(matrix[j][i]) != colSet.end()) 
+        //validate columns
+        for (int j = 0; j < n; j++) {
+            set<int> st;
+            for (int i = 0; i < m; i++) {
+                if (st.find(matrix[i][j]) != st.end()) {
                     return false;
-                colSet.insert(matrix[j][i]);
+                }
+                st.insert(matrix[i][j]);
             }
         }
 

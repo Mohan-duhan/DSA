b6c985e86bcf6f734a75ae6ae2d6ff6cbbb87a66
Mohan duhan <135856544+Mohan-duhan@users.noreply.github.com>
2025-09-03 12:43:34 +0530

    Time: 66 ms (64.95%) | Memory: 79.2 MB (53.05%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,42 +1,18 @@
 class Solution {
 public:
-    void merge(vector<int> &arr, int left, int mid, int right) {
-        int leftSize = mid - left + 1;
-        int rightSize = right - mid;
-
-        vector<int> leftSubarr(arr.begin() + left, arr.begin() + left + leftSize);
-        vector<int> rightSubarr(arr.begin() + mid + 1, arr.begin() + mid + 1 + rightSize);
-
-        int i = 0, j = 0, k = left;
-
-        while (i < leftSize && j < rightSize) {
-            if (leftSubarr[i] <= rightSubarr[j]) {
-                arr[k++] = leftSubarr[i++];
-            } else {
-                arr[k++] = rightSubarr[j++];
-            }
-        }
-
-        while (i < leftSize) {
-            arr[k++] = leftSubarr[i++];
-        }
-
-        while (j < rightSize) {
-            arr[k++] = rightSubarr[j++];
-        }
-    }
-
-    void mergeSort(vector<int> &arr, int left, int right) {
-        if (left < right) {
-            int mid = left + (right - left) / 2;
-            mergeSort(arr, left, mid);
-            mergeSort(arr, mid + 1, right);
-            merge(arr, left, mid, right);
-        }
-    }
-
     vector<int> sortArray(vector<int>& nums) {
-        mergeSort(nums, 0, nums.size() - 1);
-        return nums;
+        priority_queue<int, vector<int>, greater<int>> pq; 
+        vector<int> ans;
+
+        for (int i = 0; i < nums.size(); i++) {
+            pq.push(nums[i]);
+        }
+
+        while (!pq.empty()) {
+            ans.push_back(pq.top());
+            pq.pop();
+        }
+
+        return ans;
     }
 };

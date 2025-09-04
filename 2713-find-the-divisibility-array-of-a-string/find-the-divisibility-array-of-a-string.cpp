80449d1571563db475d7f7141e995d932d97c149
Mohan duhan <135856544+Mohan-duhan@users.noreply.github.com>
2025-09-04 13:26:09 +0530

    Time: 7 ms (60.97%) | Memory: 25.1 MB (60.00%) - LeetSync

--- 2713-find-the-divisibility-array-of-a-string/find-the-divisibility-array-of-a-string.cpp
+++ 2713-find-the-divisibility-array-of-a-string/find-the-divisibility-array-of-a-string.cpp
@@ -7,7 +7,7 @@ public:
 
         for (int i = 0; i < n; i++) {
             int num = word[i] - '0';
-            res = (res * 10 + num) % m;
+            res = (res * 10 + num) % m; 
             ans[i] = (res == 0 ? 1 : 0);
         }
         return ans;

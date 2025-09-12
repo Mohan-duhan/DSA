87587d78a213a71af79f14986de4c2d7d366a9cd
Mohan <135856544+Mohan-duhan@users.noreply.github.com>
2025-09-12 13:02:32 +0530

    Time: 7 ms (58.96%) | Memory: 19.2 MB (25.75%) - LeetSync

--- 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
@@ -1,19 +1,12 @@
 class Solution {
 public:
-    bool isVowel(char ch) {
-        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
-    }
     bool doesAliceWin(string s) {
-        int n = s.length();
-        int cnt = 0;
-        for (int i = 0; i < n; i++) {
-            if (isVowel(s[i])) {
-                cnt++;
-            }
-        }
-        if (cnt == 0)
-            return false;
-        else
+    string str="aeiou";
+        for(int i=0;i<s.size();i++)
+        {
+            if(str.find(s[i])!=string::npos)
             return true;
+        }
+        return false;
     }
 };

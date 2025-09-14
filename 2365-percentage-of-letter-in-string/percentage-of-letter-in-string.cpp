76dd5610c27c5cb969e6fada046c0fe5b982d937
Mohan <135856544+Mohan-duhan@users.noreply.github.com>
2025-09-14 16:25:42 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (36.76%) - LeetSync

--- 2365-percentage-of-letter-in-string/percentage-of-letter-in-string.cpp
+++ 2365-percentage-of-letter-in-string/percentage-of-letter-in-string.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int percentageLetter(string s, char letter) {
         int n = s.size();
-        double cnt = 0;
+        int cnt = 0;
 
         for(int i = 0;i<n;i++){
             if(s[i] == letter)cnt++;
         }
-        cnt = (cnt / n) * 100;
+        cnt = (cnt * 100) / n;
         return cnt;
     }
 };

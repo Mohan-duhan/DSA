49f8686adebc37e21f0bee567d5e597ba0f94bf8
Mohan <135856544+Mohan-duhan@users.noreply.github.com>
2025-09-21 18:47:07 +0530

    Time: 2 ms (48.61%) | Memory: 12.8 MB (38.00%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -4,6 +4,7 @@ public:
         unordered_map<int, int> next_greater;
         stack<int> st;
 
+        //yaha map form hoga for greater values
         for (int num : arr) {
             while (!st.empty() && st.top() < num) {
                 next_greater[st.top()] = num;
@@ -12,12 +13,14 @@ public:
             st.push(num);
         }
 
+        //yaha stack se pop nahi hui values ko -1 assign hoga 
         while (!st.empty()) {
             next_greater[st.top()] = -1;
             st.pop();
         }
-        vector<int> ans;
 
+        //yaha result update hoga 
+        vector<int> ans;
         for (int num : nums) {
             ans.push_back(next_greater[num]);
         }

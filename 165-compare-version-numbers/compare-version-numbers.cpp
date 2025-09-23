6633673207bb52af612b66bd8dd0e53187e5527e
Mohan <135856544+Mohan-duhan@users.noreply.github.com>
2025-09-23 19:19:10 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (5.69%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.cpp
+++ 165-compare-version-numbers/compare-version-numbers.cpp
@@ -1,35 +1,28 @@
 class Solution {
 public:
-    vector<string> getToken(string version) {
-        stringstream ss(version);
-        string word;
-        vector<string> ans;
-
-        while (getline(ss, word, '.')) {
-            ans.push_back(word);
-        }
-        return ans;
-    }
-
     int compareVersion(string version1, string version2) {
-        vector<string> vec1 = getToken(version1);
-        vector<string> vec2 = getToken(version2);
+        vector<int> v1 = getToken(version1);
+        vector<int> v2 = getToken(version2);
 
-        int n = vec1.size();
-        int m = vec2.size();
-        int i = 0;
+        int n = max(v1.size(), v2.size());
+        for (int i = 0; i < n; i++) {
+            int a = i < v1.size() ? v1[i] : 0;
+            int b = i < v2.size() ? v2[i] : 0;
 
-        while (i < n || i < m) {
-            int a = i < n ? stoi(vec1[i]) : 0;
-            int b = i < m ? stoi(vec2[i]) : 0;
-
-            if (a < b)
-                return -1;
-            else if (a > b)
-                return 1;
-            else
-                i++;
+            if (a < b) return -1;
+            if (a > b) return 1;
         }
         return 0;
+    }
+
+private:
+    vector<int> getToken(const string& version) {
+        vector<int> ans;
+        stringstream ss(version);
+        string num;
+        while (getline(ss, num, '.')) {
+            ans.push_back(stoi(num));
+        }
+        return ans;
     }
 };

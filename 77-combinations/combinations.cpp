1ea0193a5d53aa66ae8bc53311b63c3ae0c0dc6e
Mohan <135856544+Mohan-duhan@users.noreply.github.com>
2025-09-25 21:25:34 +0530

    Time: 75 ms (31.63%) | Memory: 82.4 MB (42.06%) - LeetSync

--- 77-combinations/combinations.cpp
+++ 77-combinations/combinations.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     vector<vector<int>> res;
+
     void solve(int val, int k, vector<int>& temp, int n) {
         if (k == 0) {
             res.push_back(temp);
             return;
         }
-        if (val > n)
-            return;
 
-        temp.push_back(val);
-        solve(val + 1, k - 1, temp, n);
-
-        temp.pop_back();
-        solve(val + 1, k, temp, n);
+        for (int i = val; i <= n; i++) {
+            temp.push_back(i);               // choose i
+            solve(i + 1, k - 1, temp, n);    // move forward
+            temp.pop_back();                 // backtrack
+        }
     }
+
     vector<vector<int>> combine(int n, int k) {
         vector<int> temp;
-
         solve(1, k, temp, n);
         return res;
     }

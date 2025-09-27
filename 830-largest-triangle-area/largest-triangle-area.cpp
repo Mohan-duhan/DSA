45f95775d8829db1803aeb3dba14ccb8e539cdfe
Mohan <135856544+Mohan-duhan@users.noreply.github.com>
2025-09-27 14:44:27 +0530

    Time: 4 ms (21.96%) | Memory: 10.4 MB (63.36%) - LeetSync

--- 830-largest-triangle-area/largest-triangle-area.cpp
+++ 830-largest-triangle-area/largest-triangle-area.cpp
@@ -11,15 +11,10 @@ public:
                     int x2 = points[j][0], y2 = points[j][1];
                     int x3 = points[k][0], y3 = points[k][1];
 
-                    double a = hypot(x2 - x3, y2 - y3);
-                    double b = hypot(x1 - x3, y1 - y3);
-                    double c = hypot(x1 - x2, y1 - y2);
+                    double area = 0.5 * abs(x1 * (y2 - y3) + x2 * (y3 - y1) +
+                                            x3 * (y1 - y2));
 
-                    double s = (a + b + c) / 2.0;
-
-                    double area = sqrt(max(0.0, s * (s - a) * (s - b) * (s - c)));
-
-                    maxArea = max(maxArea, area);
+                    maxArea = max(area , maxArea);
                 }
             }
         }

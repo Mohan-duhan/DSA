b604df766e0eebbf10d9ad78aaf556b532010d2d
Mohan <135856544+Mohan-duhan@users.noreply.github.com>
2026-01-03 14:13:06 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (70.00%) - LeetSync

--- 1527-number-of-ways-to-paint-n-3-grid/number-of-ways-to-paint-n-3-grid.cpp
+++ 1527-number-of-ways-to-paint-n-3-grid/number-of-ways-to-paint-n-3-grid.cpp
@@ -7,8 +7,8 @@ public:
         long long A_two = 6, B_three = 6;//set for n = 1 initially
 
         for (int i = 2; i <= n; i++) {
-            long long newB = (2 * A_two + 2 * B_three) % MOD;
-            long long newA = (3 * A_two + 2 * B_three) % MOD;
+            long long newB = (2 * A_two + 2 * B_three) % MOD;//3 se 2 me jaine ke or 3 se 2 me naine ke (all t)
+            long long newA = (3 * A_two + 2 * B_three) % MOD;//2 ae 2 me jaine ke or 2 se 3 me jaine ke 
             A_two = newA;
             B_three = newB;
         }

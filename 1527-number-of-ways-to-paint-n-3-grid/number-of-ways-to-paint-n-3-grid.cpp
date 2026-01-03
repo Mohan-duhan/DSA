03c112c3915f2c03dfc038968dbf76a3bbc9d6b7
Mohan <135856544+Mohan-duhan@users.noreply.github.com>
2026-01-03 14:11:58 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (70.00%) - LeetSync

--- 1527-number-of-ways-to-paint-n-3-grid/number-of-ways-to-paint-n-3-grid.cpp
+++ 1527-number-of-ways-to-paint-n-3-grid/number-of-ways-to-paint-n-3-grid.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     int numOfWays(int n) {
         const int MOD = 1000000007;
-        //A = number of ways to paint a FULL ROW of type ABA
-        //B = number of ways to paint a FULL ROW of type ABC
-        long long A = 6, B = 6;//set for n = 1 initially
+        //A_two = number of ways to paint a FULL ROW of type ABA
+        //B_three = number of ways to paint a FULL ROW of type ABC
+        long long A_two = 6, B_three = 6;//set for n = 1 initially
 
         for (int i = 2; i <= n; i++) {
-            long long newA = (2 * A + 2 * B) % MOD;
-            long long newB = (2 * A + 3 * B) % MOD;
-            A = newA;
-            B = newB;
+            long long newB = (2 * A_two + 2 * B_three) % MOD;
+            long long newA = (3 * A_two + 2 * B_three) % MOD;
+            A_two = newA;
+            B_three = newB;
         }
 
-        return (A + B) % MOD;
+        return (B_three + A_two) % MOD;
     }
 };

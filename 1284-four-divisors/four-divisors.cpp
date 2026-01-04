104f9a582034e20bff83a1451989a1542838e59e
Mohan <135856544+Mohan-duhan@users.noreply.github.com>
2026-01-04 13:03:23 +0530

    Time: 30 ms (49.51%) | Memory: 16.2 MB (71.43%) - LeetSync

--- 1284-four-divisors/four-divisors.cpp
+++ 1284-four-divisors/four-divisors.cpp
@@ -1,42 +1,34 @@
-class Solution { 
-public: 
-    vector<pair<int, int>> pairs; 
-
-    void solve(int x) { 
-        int sum = 1 + x; 
+class Solution {
+public:
+    int solve(int x) {
+        int sum = 1 + x;
         int cnt = 2; // 1 and x
 
-        for (int i = 2; i * i <= x; i++) { 
-            if (x % i == 0) { 
-                int d1 = i; 
-                int d2 = x / i; 
+        for (int i = 2; i * i <= x; i++) {
+            if (x % i == 0) {
+                int d1 = i;
+                int d2 = x / i;
 
-                if (d1 == d2) { 
-                    cnt++; 
-                    sum += d1; 
-                } else { 
-                    cnt += 2; 
-                    sum += d1 + d2; 
-                } 
-            } 
-        } 
+                if (d1 == d2) {
+                    cnt++;
+                    sum += d1;
+                } else {
+                    cnt += 2;
+                    sum += d1 + d2;
+                }
+            }
+        }
 
-        if (cnt == 4) { 
-            pairs.push_back({sum, cnt}); 
-        } 
-    } 
+        return cnt == 4 ? sum : 0;
+    }
 
-    int sumFourDivisors(vector<int>& nums) { 
-        int ans = 0; 
+    int sumFourDivisors(vector<int>& nums) {
+        int ans = 0;
 
-        for (int x : nums) { 
-            solve(x); 
-        } 
+        for (int x : nums) {
+            ans += solve(x);
+        }
 
-        for (auto &p : pairs) { 
-            ans += p.first; 
-        } 
-
-        return ans; 
-    } 
+        return ans;
+    }
 };
